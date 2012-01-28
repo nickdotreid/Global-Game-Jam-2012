35569ad09fa6933598a31b29737a5140ab981a8c
Nick Reid <nickreid@nickreid.com>
2012-01-28 10:34:05 -0800

    added location tracking to each page

--- app.py
+++ app.py
@@ -12,7 +12,24 @@ def home_page():
 	login_phone()
 	return render_template("pages/home_page.html")
 	
+@app.route("/track",methods=['GET','POST'])
+def view_track_location():
+	login_phone()
+	if track_location():
+		return jsonify({'success':True})
+	return jsonify({'success':False})
+
+def track_location():
+	if 'lat' in request.form and 'lng' in request.form:
+		location = Location(session['player'],request.form['lat'],request.form['lng'])
+		db_session.add(location)
+		db_session.commit()
+		return True
+	return False
+	
 def login_phone():
+	if session['player']:
+		return session['player']
 	if 'phone' in request.args:
 		# check if number valid
 		player = Player.query.filter_by(phone=request.args['phone']).first()
