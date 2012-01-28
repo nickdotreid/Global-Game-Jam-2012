0dd22101c9edbe0c406001b6e284b20c5bbaf56c
Nick Reid <nickreid@nickreid.com>
2012-01-28 09:21:26 -0800

    added simple login

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, redirect, request, flash, render_template, session, jsonify, Blueprint
 import twilio
 
-app = Flask(__name__)
+from database import db_session
+from models import *
 
-@app.route("/")
+app = Flask(__name__)
+app.secret_key = 'secrect'
+
+@app.route("/",methods=['GET', 'POST'])
 def home_page():
+	login_phone()
 	return render_template("pages/home_page.html")
+	
+def login_phone():
+	if 'phone' in request.args:
+		# check if number valid
+		player = Player.query.filter_by(phone=request.args['phone']).first()
+		if player is None:
+			player = Player(request.args['phone'])
+			db_session.add(player)
+			db_session.commit()
+		session['player'] = player
