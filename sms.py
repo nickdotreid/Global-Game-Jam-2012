852db3a883cf2543d935db6018856b8cd5670949
Nick Reid <nickreid@nickreid.com>
2013-02-27 00:23:23 -0800

    updated sms number

--- sms.py
+++ sms.py
@@ -10,7 +10,7 @@ def send_sms(number,message):
 		number = twilio_format_number(number)
 		client = TwilioRestClient(twilio_account_sid, twilio_auth_token)
 		message = client.sms.messages.create(to=number,
-		                                     from_="+14157023723",
+		                                     from_="+15107358741",
 		                                     body=message)
 	except:
 		pass
